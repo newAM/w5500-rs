0efa3836460ca715a1a302effff05d109c4768b3
Alex M <alexmgit@protonmail.com>
2021-02-07 12:35:45 -0800

    Add on-target tests.

---
+++ testsuite-peer/src/main.rs
@@ -0,0 +1,77 @@
+use reqwest::{Response, StatusCode, Version};
+use testsuite_assets::{CHUNK_SIZE, NUM_CHUNKS, W5500_IP, W5500_UDP_PORT};
+use tokio::{
+    io::AsyncReadExt,
+    net::{TcpListener, UdpSocket},
+};
+
+#[tokio::main]
+async fn main() {
+    {
+        let url = format!("http://{}", W5500_IP);
+        println!("Sending HTTP GET request to: {}", url);
+        let response: Response = reqwest::get(&url).await.unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_eq!(response.version(), Version::HTTP_11);
+        assert_eq!(response.headers().get("Server").unwrap(), "W5500");
+        assert_eq!(
+            response.headers().get("Content-Type").unwrap(),
+            "text/plain;charset=UTF-8"
+        );
+        assert_eq!(response.text().await.unwrap(), "Hello World");
+        println!("HTTP GET test PASSED");
+    }
+
+    {
+        let local_addr = format!(
+            "{}:{}",
+            testsuite_assets::PEER_IP,
+            testsuite_assets::PEER_TCP_PORT
+        );
+        println!("Listening on {}", local_addr);
+        let listener = TcpListener::bind(&local_addr).await.unwrap();
+        let (mut socket, _) = listener.accept().await.unwrap();
+
+        for i in 0..NUM_CHUNKS {
+            println!("Chunk {}", i);
+            let mut buf = vec![0; CHUNK_SIZE];
+            socket.read_exact(&mut buf).await.unwrap();
+            for idx in 0..buf.len() {
+                assert_eq!(buf[idx], idx as u8);
+            }
+        }
+        println!("TCP client tests PASSED");
+    }
+
+    {
+        let local_addr = format!(
+            "{}:{}",
+            testsuite_assets::PEER_IP,
+            testsuite_assets::PEER_UDP_PORT
+        );
+        println!("Binding a UDP socket to {}", local_addr);
+        let socket = UdpSocket::bind(&local_addr).await.unwrap();
+        let mut buf = vec![0; 2048];
+        for _ in 0..2usize {
+            let (n, from) = socket.recv_from(&mut buf).await.unwrap();
+            assert_eq!(&buf[..n], testsuite_assets::UDP_DATA);
+            assert_eq!(from.port(), W5500_UDP_PORT);
+            let ip = match from.ip() {
+                std::net::IpAddr::V4(ip) => ip,
+                std::net::IpAddr::V6(_) => {
+                    panic!("Unexpected IPv6")
+                }
+            };
+            assert_eq!(ip.octets(), W5500_IP.octets);
+        }
+
+        let w5500_addr = format!("{}:{}", W5500_IP, W5500_UDP_PORT);
+        println!("Sending data to {}", w5500_addr);
+        socket
+            .send_to(testsuite_assets::UDP_DATA, &w5500_addr)
+            .await
+            .unwrap();
+    }
+
+    println!("Done all tests");
+}
---
+++ testsuite/README.md
@@ -0,0 +1,54 @@
+# W5500 Testsuite
+These are on-target tests for the W5500.
+
+The purpose of these tests is to validate the `w5500-hl` crate with real hardware.
+
+## Setup
+Like all embedded development this takes quite a bit of effort to setup.
+
+This uses some custom hardware, ([ambientsensor]), but this should be easy to adapt for another Cortex-M platform.
+
+To make this work for another Cortex-M platform you should modify:
+
+* `memory.x`
+* `testsuite/Cargo.toml`
+    * Replace the `stm32f0xx-hal` with the appropriate HAL for your device.
+* `testsuite/tests/tests.rs`
+    * The W5500 pin setup and mapping.
+* `testsuite-assets/src/lib.rs`
+    * Update addresses and ports to fit your network.
+
+### Software Setup
+I will assume you have rust (and your desired targets) installed.
+
+* defmt-test (source: [app-template])
+    * `cargo install flip-link`
+    * `cargo install probe-run`
+
+## How it works
+There are two sides to this, the embedded W5500 and a peer for it to communicate with (your PC).
+
+### W5500 Side
+These tests run with [defmt-test], an embedded test framework.
+
+The W5500 IP, Gateway, MAC, and subnet mask are all hard coded in the `testsuite-assets/src/lib.rs` file.
+
+### Peer Side
+To make the tests worthwhile the W5500 has to communicate with something.
+
+The other side of this is another workspace member, `testsuite-peer`.
+
+## Running
+In one session start the W5500 side:
+```bash
+cargo test -p testsuite --target thumbv6m-none-eabi
+```
+
+Wait for the W5500 to start executing the first test, then in another session start the peer script:
+```bash
+cargo run -p testsuite-peer
+```
+
+[app-template]: https://github.com/knurling-rs/app-template
+[defmt-test]: https://defmt.ferrous-systems.com/
+[ambientsensor]: https://github.com/newam/ambientsensor
