a1f6ae1d4bba945f891be83a5656eb737b527569
Alex Martens <alex@thinglab.org>
2022-10-21 20:59:03 -0700

    ll: use async_fn_in_trait

--- ll/src/eh1/vdm.rs
+++ ll/src/eh1/vdm.rs
@@ -102,33 +102,25 @@ where
     /// SPI IO error type.
     type Error = E;
 
-    type ReadFuture<'a> = impl core::future::Future<Output = Result<(), Self::Error>> + 'a where Self: 'a;
-
     /// Read from the W5500 asynchronously.
-    fn read<'a>(&'a mut self, address: u16, block: u8, data: &'a mut [u8]) -> Self::ReadFuture<'a> {
-        async move {
-            let header = vdm_header(address, block, AccessMode::Read);
-            eha0::spi::SpiDevice::transaction(&mut self.spi, move |bus| async move {
-                let bus = unsafe { &mut *bus };
-                bus.write(&header).await?;
-                bus.read(data).await
-            })
-            .await
-        }
+    async fn read(&mut self, address: u16, block: u8, data: &mut [u8]) -> Result<(), Self::Error> {
+        let header = vdm_header(address, block, AccessMode::Read);
+        eha0::spi::SpiDevice::transaction(&mut self.spi, move |bus| async move {
+            let bus = unsafe { &mut *bus };
+            bus.write(&header).await?;
+            bus.read(data).await
+        })
+        .await
     }
 
-    type WriteFuture<'a> = impl core::future::Future<Output = Result<(), Self::Error>> + 'a where Self: 'a;
-
     /// Write to the W5500 asynchronously.
-    fn write<'a>(&'a mut self, address: u16, block: u8, data: &'a [u8]) -> Self::WriteFuture<'a> {
-        async move {
-            let header = vdm_header(address, block, AccessMode::Write);
-            eha0::spi::SpiDevice::transaction(&mut self.spi, move |bus| async move {
-                let bus = unsafe { &mut *bus };
-                bus.write(&header).await?;
-                bus.write(data).await
-            })
-            .await
-        }
+    async fn write(&mut self, address: u16, block: u8, data: &[u8]) -> Result<(), Self::Error> {
+        let header = vdm_header(address, block, AccessMode::Write);
+        eha0::spi::SpiDevice::transaction(&mut self.spi, move |bus| async move {
+            let bus = unsafe { &mut *bus };
+            bus.write(&header).await?;
+            bus.write(data).await
+        })
+        .await
     }
 }
