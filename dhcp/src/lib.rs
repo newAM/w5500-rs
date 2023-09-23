newAM/w5500-rs

commit 3cb6a20f85561e716a18c04a32d523046a88a4ae

DHCP: add ServerId in REQUEST message
* dhcp: add ServerId in REQUEST message

fixes #249

* dhcp: add changelog entry

dhcp/src/lib.rs
@@ -129,6 +129,8 @@ pub struct Client<'a> {
     lease: u32,
     /// Time that the lease was obtained.
     lease_monotonic_secs: u32,
+    /// Last DHCP server
+    server: Option<Ipv4Addr>,
     /// Last XID
     xid: u32,
     /// XID generator
@@ -184,6 +186,7 @@ impl<'a> Client<'a> {
             t2: 0,
             lease: 0,
             lease_monotonic_secs: 0,
+            server: None,
             xid: rand.next_u32(),
             rand,
             mac,
@@ -422,6 +425,7 @@ impl<'a> Client<'a> {
             match self.state {
                 State::Selecting => {
                     self.ip = pkt.yiaddr()?;
+                    self.server = Some(pkt.siaddr()?);
                     pkt.done()?;
                     self.request(w5500)?;
                     self.set_state_with_timeout(State::Requesting, monotonic_secs);
@@ -590,7 +594,15 @@ impl<'a> Client<'a> {
     fn request<W5500: Registers>(&mut self, w5500: &mut W5500) -> Result<(), Error<W5500::Error>> {
         self.xid = self.rand.next_u32();
         debug!("sending DHCPREQUEST xid={:08X}", self.xid);
-        send_dhcp_request(w5500, self.sn, &self.mac, &self.ip, self.hostname, self.xid)?;
+        send_dhcp_request(
+            w5500,
+            self.sn,
+            &self.mac,
+            &self.ip,
+            self.hostname,
+            self.server.as_ref().unwrap(),
+            self.xid,
+        )?;
         Ok(())
     }
 

dhcp/src/pkt.rs
@@ -310,6 +310,11 @@ impl<'a, W: Registers> PktSer<'a, W> {
         self.writer.write_all(&ip.octets())
     }
 
+    fn set_option_server_id(&mut self, ip: &Ipv4Addr) -> Result<(), Error<W::Error>> {
+        self.writer.write_all(&[Options::ServerId.into(), 4])?;
+        self.writer.write_all(&ip.octets())
+    }
+
     fn set_option_end(&mut self) -> Result<(), Error<W::Error>> {
         self.writer.write_all(&[Options::End.into()])
     }
@@ -335,6 +340,7 @@ impl<'a, W: Registers> PktSer<'a, W> {
         mac: &Eui48Addr,
         ip: &Ipv4Addr,
         hostname: Hostname,
+        server: &Ipv4Addr,
         xid: u32,
     ) -> Result<UdpWriter<'a, W>, Error<W::Error>> {
         self.prepare_message(mac, xid)?;
@@ -343,6 +349,7 @@ impl<'a, W: Registers> PktSer<'a, W> {
         self.set_option_hostname(hostname)?;
         self.set_option_parameter_request()?;
         self.set_option_requested_ip(ip)?;
+        self.set_option_server_id(server)?;
         self.set_option_end()?;
         Ok(self.writer)
     }
@@ -369,11 +376,12 @@ pub fn send_dhcp_request<W: Registers>(
     mac: &Eui48Addr,
     ip: &Ipv4Addr,
     hostname: Hostname,
+    server: &Ipv4Addr,
     xid: u32,
 ) -> Result<(), Error<W::Error>> {
     let writer: UdpWriter<W> = w5500.udp_writer(sn)?;
     PktSer::from(writer)
-        .dhcp_request(mac, ip, hostname, xid)?
+        .dhcp_request(mac, ip, hostname, server, xid)?
         .send()?;
     Ok(())
 }
@@ -429,6 +437,14 @@ impl<'a, W: Registers> PktDe<'a, W> {
     /// know its own address (ciaddr was 0).
     pub fn yiaddr(&mut self) -> Result<Ipv4Addr, Error<W::Error>> {
         self.reader.seek(SeekFrom::Start(16))?;
+        let mut buf: [u8; 4] = [0; 4];
+        self.reader.read_exact(&mut buf)?;
+        Ok(buf.into())
+    }
+
+    /// (next) server IP address
+    pub fn siaddr(&mut self) -> Result<Ipv4Addr, Error<W::Error>> {
+        self.reader.seek(SeekFrom::Start(20))?;
         let mut buf: [u8; 4] = [0; 4];
         self.reader.read_exact(&mut buf)?;
         Ok(buf.into())

commit 3acd54296b6f7708a6b5fb56569eb5692d618b3b

dhcp: fix server identifier in REQUEST message
Currently, the server identifier added in the REQUEST message
corresponds to the siaddr retrieved in the OFFER. This is probably
correct in some situations, but I noticed this does not work in my case:
the REQUEST is NACK'ed by the server.

The DHCP server of my Internet box sends an OFFER containing 0.0.0.0 as
next server IP, and its address (192.168.1.254) in the DHCP server
identifier option (54).

From RFC2131, the next server IP (siaddr) is indeed not supposed to be
used like this:

> DHCP clarifies the interpretation of the 'siaddr' field as the
> address of the server to use in the next step of the client's
> bootstrap process. A DHCP server may return its own address in the
> 'siaddr' field, if the server is prepared to supply the next
> bootstrap service (e.g., delivery of an operating system executable
> image). A DHCP server always returns its own address in the 'server
> identifier' option.

Additionally, in dhclient sources (see the 2nd link), we can see
that next_srv_addr (filled from siaddr) is just passed to the script.
On the other hand, the server identifier option is added in the REQUEST
if it was present in the OFFER.

This patch tries to imitate this behavior: if the option was present in
the OFFER, it will be added in the REQUEST.

Fixes: 3cb6a20f8556 ("DHCP: add ServerId in REQUEST message")
Link: https://www.rfc-editor.org/rfc/rfc2131.txt
Link: https://github.com/isc-projects/dhcp/blob/master/client/dhclient.c
Signed-off-by: Olivier Matz <zer0@droids-corp.org>

dhcp/src/lib.rs
@@ -129,8 +129,8 @@ pub struct Client<'a> {
     lease: u32,
     /// Time that the lease was obtained.
     lease_monotonic_secs: u32,
-    /// Last DHCP server
-    server: Option<Ipv4Addr>,
+    /// DHCP server identifier option
+    server_id_option: Option<Ipv4Addr>,
     /// Last XID
     xid: u32,
     /// XID generator
@@ -186,7 +186,7 @@ impl<'a> Client<'a> {
             t2: 0,
             lease: 0,
             lease_monotonic_secs: 0,
-            server: None,
+            server_id_option: None,
             xid: rand.next_u32(),
             rand,
             mac,
@@ -425,7 +425,7 @@ impl<'a> Client<'a> {
             match self.state {
                 State::Selecting => {
                     self.ip = pkt.yiaddr()?;
-                    self.server = Some(pkt.siaddr()?);
+                    self.server_id_option = pkt.dhcp_server()?;
                     pkt.done()?;
                     self.request(w5500)?;
                     self.set_state_with_timeout(State::Requesting, monotonic_secs);
@@ -600,7 +600,7 @@ impl<'a> Client<'a> {
             &self.mac,
             &self.ip,
             self.hostname,
-            self.server.as_ref().unwrap(),
+            self.server_id_option.as_ref(),
             self.xid,
         )?;
         Ok(())

dhcp/src/pkt.rs
@@ -340,7 +340,7 @@ impl<'a, W: Registers> PktSer<'a, W> {
         mac: &Eui48Addr,
         ip: &Ipv4Addr,
         hostname: Hostname,
-        server: &Ipv4Addr,
+        server_id_option: Option<&Ipv4Addr>,
         xid: u32,
     ) -> Result<UdpWriter<'a, W>, Error<W::Error>> {
         self.prepare_message(mac, xid)?;
@@ -349,7 +349,9 @@ impl<'a, W: Registers> PktSer<'a, W> {
         self.set_option_hostname(hostname)?;
         self.set_option_parameter_request()?;
         self.set_option_requested_ip(ip)?;
-        self.set_option_server_id(server)?;
+        if let Some(server_id) = server_id_option {
+            self.set_option_server_id(server_id)?;
+        }
         self.set_option_end()?;
         Ok(self.writer)
     }
@@ -376,12 +378,12 @@ pub fn send_dhcp_request<W: Registers>(
     mac: &Eui48Addr,
     ip: &Ipv4Addr,
     hostname: Hostname,
-    server: &Ipv4Addr,
+    server_id_option: Option<&Ipv4Addr>,
     xid: u32,
 ) -> Result<(), Error<W::Error>> {
     let writer: UdpWriter<W> = w5500.udp_writer(sn)?;
     PktSer::from(writer)
-        .dhcp_request(mac, ip, hostname, server, xid)?
+        .dhcp_request(mac, ip, hostname, server_id_option, xid)?
         .send()?;
     Ok(())
 }
@@ -437,14 +439,6 @@ impl<'a, W: Registers> PktDe<'a, W> {
     /// know its own address (ciaddr was 0).
     pub fn yiaddr(&mut self) -> Result<Ipv4Addr, Error<W::Error>> {
         self.reader.seek(SeekFrom::Start(16))?;
-        let mut buf: [u8; 4] = [0; 4];
-        self.reader.read_exact(&mut buf)?;
-        Ok(buf.into())
-    }
-
-    /// (next) server IP address
-    pub fn siaddr(&mut self) -> Result<Ipv4Addr, Error<W::Error>> {
-        self.reader.seek(SeekFrom::Start(20))?;
         let mut buf: [u8; 4] = [0; 4];
         self.reader.read_exact(&mut buf)?;
         Ok(buf.into())